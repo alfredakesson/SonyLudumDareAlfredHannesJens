1eb61114e8730107a2432225cb5c91e992d99935
Hannes Johansson <hannes.johansson91@gmail.com>
2014-04-26 02:35:41 +0200

    Made Entity X and Y tweenable

--- theGame/src/com/supercoolnamespace/hackgame/Entity.java
+++ theGame/src/com/supercoolnamespace/hackgame/Entity.java
@@ -4,5 +4,21 @@ public abstract class Entity {
 	float x; 
 	float y; 
 	
+	public void setX(float newX){
+		this.x = newX;
+	}
+	
+	public void setY(float newY){
+		this.y = newY;
+	}
+	
+	public float getX(){
+		return x;
+	}
+	
+	public float getY(){
+		return y;
+	}
+	
 	public abstract void draw();
 }
