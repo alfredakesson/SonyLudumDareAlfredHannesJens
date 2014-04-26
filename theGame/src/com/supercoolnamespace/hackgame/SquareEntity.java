cd43d6d9a9bef65fed7eb72243eb4767787680b6
jensgustafsson <jensgustafsson@live.com>
2014-04-26 02:12:22 +0200

    recreated the dir...

--- theGame/src/com/supercoolnamespace/hackgame/SquareEntity.java
+++ theGame/src/com/supercoolnamespace/hackgame/SquareEntity.java
@@ -2,9 +2,14 @@ package com.supercoolnamespace.hackgame;
 
 public class SquareEntity extends Entity{
 
+	public SquareEntity(float x, float y) {
+		this.x = x; 
+		this.y = y; 
+	}
+	
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
