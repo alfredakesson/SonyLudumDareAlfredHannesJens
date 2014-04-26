29ac8d2ba30b0edf30aeb675f767da5be955ba67
jensgustafsson <jensgustafsson@live.com>
2014-04-26 02:05:44 +0200

    Added a blue tiny square

--- theGame/src/com/supercoolnamespace/hackgame/MainActivity.java
+++ theGame/src/com/supercoolnamespace/hackgame/MainActivity.java
@@ -18,13 +18,17 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		// requesting to turn the title OFF
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
 		// making it full screen
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		// set our MainGamePanel as the View
 		setContentView(new MainGamePanel(this));
+		
 		Log.d(TAG, "View added");
 	}
 
