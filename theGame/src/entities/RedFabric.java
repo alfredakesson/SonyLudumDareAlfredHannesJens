f138b05ef4df373d50f5f4b52b222af90436b332
Hannes Johansson <hannes.johansson91@gmail.com>
2014-04-27 15:51:01 +0200

    updated house color

--- theGame/src/entities/RedFabric.java
+++ theGame/src/entities/RedFabric.java
@@ -17,11 +17,11 @@ public class RedFabric extends Entity {
 	@Override
 	public void draw(Canvas c, Offset theDrawOffset) {
 		Paint p = new Paint();
-		p.setColor(Color.GREEN);
+		p.setColor(Color.rgb(20, 20, 20));
 		c.drawRect(x, y, x+300, y+200, p);
 		p = new Paint();
-		p.setColor(Color.RED);
-		c.drawRect(x+40, y+50, x+180, y+150, p);
+		//p.setColor(Color.RED);
+		//c.drawRect(x+40, y+50, x+180, y+150, p);
 	}
 
 }
